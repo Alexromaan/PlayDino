9c00f8439fe298fc7ca7a6da1f2cf5dea4ba3cc6
xt3m1x <66008237+xt3m1x@users.noreply.github.com>
2021-04-13 08:05:08 +0200

    ya funciionan los formularios

--- mysite/apps/login/urls.py
+++ mysite/apps/login/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-
 app_name = 'login'
 
 urlpatterns = [
-    path('', views.login_home, name='mainlogin'),
+    path('', views.mainlogin, name='mainlogin'),
     path('desconectar/', views.logout_home, name='disconnect'),
     path('registro', views.RegistroUsuario ,name='registro'),
 ]
--- mysite/apps/login/views.py
+++ mysite/apps/login/views.py
@@ -1,44 +1,37 @@
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 from django.urls import reverse
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, LoginForm
 
-def login_home(request):
-    if request.method == 'GET':
-        if not request.user.is_authenticated:
-            return render(request, 'login.html')
-
-        if request.user.is_authenticated:
-            return HttpResponseRedirect(reverse('mainpage:home'))
-
+def login(request):
+    data = {
+        'form' : LoginForm(),
+    }
     if request.method == 'POST':
 
-        user = authenticate(username=request.POST['username'], password=request.POST['password'])
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse('login.html'))
-
+        formulario = LoginForm(data=request.POST)
+        data['form'] = formulario
+    #mainpage return render(request, 'mainpage:.html', data)
 
 def logout_home(request):
     logout(request)
-    return HttpResponseRedirect(reverse('login:mainlogin'))
+    return HttpResponseRedirect(reverse('login:login'))
 
 
 def RegistroUsuario(request):
     data = {
-        'form' : CustomUserCreationForm()
+        'form' : CustomUserCreationForm(),
     }
     if request.method == 'POST':
+
         formulario = CustomUserCreationForm(data=request.POST)
         if formulario.is_valid():
             formulario.save()
             user = authenticate(username=formulario.cleaned_data['username'], password=formulario.cleaned_data['password1'])
             login(request, user)
-            messages.success(request, "te has registrado correctamente")
             return redirect(to='login')
         data['form'] = formulario
     return render(request, 'registro.html', data)
